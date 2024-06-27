305357dd4e821975f6415247c2fadd19f7f7989e
Tobias Lindenbauer <tobias.lindenbauer@jetbrains.com>
2024-06-27 13:05:52 +0200

    Bugfix: Variable init and exception propagation

--- src/main.py
+++ src/main.py
@@ -15,7 +15,8 @@ if __name__ == '__main__':
     # Set working dir inside repo
 
     python_repositories_metadata = pd.read_csv(os.path.join(path_to_data, 'python_repos.csv'))
-    for _, repository_metadata in python_repositories_metadata.iloc[1:2].iterrows():
+    repo_instance = None
+    for _, repository_metadata in python_repositories_metadata.iloc[6:7].iterrows():
         repository_path = os.path.join(path_to_repositories, "__".join(repository_metadata["name"].split("/")))
         try:
             repo_instance = Repo.clone_from(f'https://github.com/{repository_metadata["name"]}.git',
@@ -25,11 +26,14 @@ if __name__ == '__main__':
             if 'already exists' in e.stderr:
                 print('Repository already exists, using local directory instead of cloning.')
                 repo_instance = Repo(repository_path)
+            else:
+                raise e
 
         os.chdir(os.path.join(path_to_data, repository_path))
     #os.chdir(os.path.join(path_to_repositories, 'demo-repo'))
 
-        repo_scraper = RepositoryDataScraper(repository=repo_instance, sliding_window_size=2)
+        repo_scraper = RepositoryDataScraper(repository=repo_instance, programming_language=ProgrammingLanguage.PYTHON,
+                                             sliding_window_size=2)
         repo_scraper.compute_file_commit_grams()
 
         print(f'Stats for repository {repository_metadata["name"]}:\n'
