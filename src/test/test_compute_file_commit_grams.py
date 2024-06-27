c07a68ce0622416373b049cee6454f8b9cb6615d
Tobias Lindenbauer <tobias.lindenbauer@jetbrains.com>
2024-06-27 11:26:47 +0200

    Update: File-Commit gram Test ground truth
    Our new approach already de-duplicates by default, since we dont collect the same or subsets of a gram over multiple branches, but only a single one.

--- src/test/test_compute_file_commit_grams.py
+++ src/test/test_compute_file_commit_grams.py
@@ -16,24 +16,27 @@ class ComputeFileCommitGramsTestCase(unittest.TestCase):
         self.repository_data_scraper = RepositoryDataScraper(repository=demo_repo, sliding_window_size=2)
 
     def test_should_generate_target_file_commit_grams(self):
-        target_file_commit_grams = [
-            {'file': 'document2.txt', 'branch': 'master', 'first_commit': 'aeeab817a1bd7d146fc7596546e0c98a0ec94dbc',
-             'last_commit': '6cd3cd82bfa90b80e9435513a06014ec898de4a2', 'times_seen_consecutively': 3},
-            {'file': 'document.txt', 'branch': 'master', 'first_commit': 'cf99230146d4be91004b0a68c17c69ce65945ad2',
-             'last_commit': 'a1bc309f890d0f20426859ec02c9fe4839c27559', 'times_seen_consecutively': 3},
-            {'file': 'document2.txt', 'branch': 'compliance-doc2',
-             'first_commit': '464e49457108e9685e0634c9da87254a85c06c07',
-             'last_commit': '533d06d3173710a8d1cb15b823cb7f9dcf72d536', 'times_seen_consecutively': 3},
-            {'file': 'document2.txt', 'branch': 'document2', 'first_commit': '7821ce308f797eeec65da787b96c829238e15d11',
-             'last_commit': '533d06d3173710a8d1cb15b823cb7f9dcf72d536', 'times_seen_consecutively': 5},
-            {'file': 'document2.txt', 'branch': 'document2-bugfixes',
-             'first_commit': 'e7f94bfac56abc6a0b408fee81c018fd220030f0',
-             'last_commit': '533d06d3173710a8d1cb15b823cb7f9dcf72d536', 'times_seen_consecutively': 3},
-            {'file': 'document2.txt', 'branch': 'master', 'first_commit': '618b1cb31ac5069b193603e09e3147472166b663',
-             'last_commit': '533d06d3173710a8d1cb15b823cb7f9dcf72d536', 'times_seen_consecutively': 2},
-            {'file': 'document.txt', 'branch': 'doc-style-experimentation',
+        target_file_commit_grams = [{
+            'file': 'document2.txt',
+            'branch': 'compliance-doc2',
+            'first_commit': '464e49457108e9685e0634c9da87254a85c06c07',
+            'last_commit': '533d06d3173710a8d1cb15b823cb7f9dcf72d536',
+            'times_seen_consecutively': 3},
+            {'file': 'document2.txt',
+             'branch': 'master',
+             'first_commit': 'aeeab817a1bd7d146fc7596546e0c98a0ec94dbc',
+             'last_commit': '6cd3cd82bfa90b80e9435513a06014ec898de4a2',
+             'times_seen_consecutively': 2},
+            {'file': 'document.txt',
+             'branch': 'doc-style-experimentation',
              'first_commit': 'cf99230146d4be91004b0a68c17c69ce65945ad2',
-             'last_commit': '025e1062182f5ecb404767c17180310923b0f134', 'times_seen_consecutively': 4}]
+             'last_commit': '025e1062182f5ecb404767c17180310923b0f134',
+             'times_seen_consecutively': 4},
+            {'file': 'document2.txt',
+             'branch': 'document2',
+             'first_commit': '7821ce308f797eeec65da787b96c829238e15d11',
+             'last_commit': '533d06d3173710a8d1cb15b823cb7f9dcf72d536',
+             'times_seen_consecutively': 4}]
 
         self.repository_data_scraper.compute_file_commit_grams()
         candidate_file_commit_grams = self.repository_data_scraper.accumulator
