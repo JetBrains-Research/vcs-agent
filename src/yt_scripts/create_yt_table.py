55eb4b385a2197ba26ce7cd95960a85c7328a072
Tobias Lindenbauer <tobias.lindenbauer@jetbrains.com>
2024-07-18 09:54:44 +0200

    Update: Create VQL table with all repository metadata
    Since the different datasets (per programming language) had different datatypes because of different missing values in different columns, I decided it was easiest to just consolidate the dataset and add a programmingLanguage column denoting the original dataset split. Also added scrapedData column to populate with scraping results.

--- src/yt_scripts/create_yt_table.py
+++ src/yt_scripts/create_yt_table.py
@@ -6,10 +6,20 @@ import os
 yt_client = yt.YtClient("neumann.yt.nebius.yt", token=os.environ["YT_TOKEN"])
 path_to_data = os.path.join(os.getcwd(), 'data')
 
-df = pd.read_csv(os.path.join(path_to_data, 'kotlin_repos.csv'))
-df[['topics', 'homepage', 'languages', 'labels']] = df[['topics', 'homepage', 'languages', 'labels']].replace(float('nan'), None)
+input_dfs = []
+for input_file in ['kotlin_repos.csv', 'java_repos.csv', 'python_repos.csv']:
+    df = pd.read_csv(os.path.join(path_to_data, input_file))
+    df['programmingLanguage'] = input_file.split('_')[0]
+    input_dfs.append(df)
 
-dst_table = "//home/ml4se/tobias_lindenbauer/data/kotlin_repos"
+df = pd.concat(input_dfs, ignore_index=True)
+df['scrapedData'] = None
+
+# Ensure missing values in UTF-8 objects are encoded as "None" instead of nan from float.
+object_columns = list(df.select_dtypes(include=[object]).columns)
+df[object_columns] = df[object_columns].replace(float('nan'), None)
+
+dst_table = "//home/ml4se/tobias_lindenbauer/data/repositories_to_scrape"
 dst_table_path = yt.TablePath(
     dst_table,
     schema=yt.schema.TableSchema()
@@ -33,11 +43,11 @@ dst_table_path = yt.TablePath(
     .add_column("updatedAt", ti.Optional[ti.String])
     .add_column("totalIssues", ti.Optional[ti.Double])
     .add_column("openIssues", ti.Optional[ti.Double])
-    .add_column("totalPullRequests", ti.Optional[ti.Int64])
-    .add_column("openPullRequests", ti.Optional[ti.Int64])
-    .add_column("blankLines", ti.Optional[ti.Int64])
-    .add_column("codeLines", ti.Optional[ti.Int64])
-    .add_column("commentLines", ti.Optional[ti.Int64])
+    .add_column("totalPullRequests", ti.Optional[ti.Double])
+    .add_column("openPullRequests", ti.Optional[ti.Double])
+    .add_column("blankLines", ti.Optional[ti.Double])
+    .add_column("codeLines", ti.Optional[ti.Double])
+    .add_column("commentLines", ti.Optional[ti.Double])
     .add_column("metrics", ti.Optional[ti.Utf8])
     .add_column("lastCommit", ti.Optional[ti.Utf8])
     .add_column("lastCommitSHA", ti.Optional[ti.Utf8])
@@ -48,6 +58,8 @@ dst_table_path = yt.TablePath(
     .add_column("languages", ti.Optional[ti.Utf8])
     .add_column("labels", ti.Optional[ti.Utf8])
     .add_column("topics", ti.Optional[ti.Utf8])
+    .add_column("programmingLanguage", ti.Optional[ti.Utf8])
+    .add_column("scrapedData", ti.Optional[ti.Utf8])
 )
 
 yt_client.write_table(
