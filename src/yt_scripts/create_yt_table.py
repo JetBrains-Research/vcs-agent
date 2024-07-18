727f3344c2419fce38307e017f9a335bf71d9668
Tobias Lindenbauer <tobias.lindenbauer@jetbrains.com>
2024-07-18 10:52:07 +0200

    Add: RepositoryDataRow dataclass and simplify table creation
    A new file, schemas.py, has been added to implement the RepositoryDataRow dataclass. This encapsulates all the attributes of a repository row in our tables. This change simplifies the creation of the table in create_yt_table.py by referring to the dataclass directly instead of manually declaring each column and its type.

--- src/yt_scripts/create_yt_table.py
+++ src/yt_scripts/create_yt_table.py
@@ -1,7 +1,10 @@
 import yt.wrapper as yt
-import yt.type_info as ti
+from yt.wrapper.schema import TableSchema
+
 import pandas as pd
 import os
+
+from schemas import RepositoryDataRow
 
 yt_client = yt.YtClient("neumann.yt.nebius.yt", token=os.environ["YT_TOKEN"])
 path_to_data = os.path.join(os.getcwd(), 'data')
@@ -22,44 +25,7 @@ df[object_columns] = df[object_columns].replace(float('nan'), None)
 dst_table = "//home/ml4se/tobias_lindenbauer/data/repositories_to_scrape"
 dst_table_path = yt.TablePath(
     dst_table,
-    schema=yt.schema.TableSchema()
-    .add_column("id", ti.Optional[ti.Int64])
-    .add_column("name", ti.Optional[ti.Utf8])
-    .add_column("isFork", ti.Optional[ti.Bool])
-    .add_column("commits", ti.Optional[ti.Int64])
-    .add_column("branches", ti.Optional[ti.Int64])
-    .add_column("releases", ti.Optional[ti.Int64])
-    .add_column("forks", ti.Optional[ti.Int64])
-    .add_column("mainLanguage", ti.Optional[ti.Utf8])
-    .add_column("defaultBranch", ti.Optional[ti.Utf8])
-    .add_column("license", ti.Optional[ti.Utf8])
-    .add_column("homepage", ti.Optional[ti.Utf8])
-    .add_column("watchers", ti.Optional[ti.Int64])
-    .add_column("stargazers", ti.Optional[ti.Int64])
-    .add_column("contributors", ti.Optional[ti.Int64])
-    .add_column("size", ti.Optional[ti.Int64])
-    .add_column("createdAt", ti.Optional[ti.String])
-    .add_column("pushedAt", ti.Optional[ti.String])
-    .add_column("updatedAt", ti.Optional[ti.String])
-    .add_column("totalIssues", ti.Optional[ti.Double])
-    .add_column("openIssues", ti.Optional[ti.Double])
-    .add_column("totalPullRequests", ti.Optional[ti.Double])
-    .add_column("openPullRequests", ti.Optional[ti.Double])
-    .add_column("blankLines", ti.Optional[ti.Double])
-    .add_column("codeLines", ti.Optional[ti.Double])
-    .add_column("commentLines", ti.Optional[ti.Double])
-    .add_column("metrics", ti.Optional[ti.Utf8])
-    .add_column("lastCommit", ti.Optional[ti.Utf8])
-    .add_column("lastCommitSHA", ti.Optional[ti.Utf8])
-    .add_column("hasWiki", ti.Optional[ti.Bool])
-    .add_column("isArchived", ti.Optional[ti.Bool])
-    .add_column("isDisabled", ti.Optional[ti.Bool])
-    .add_column("isLocked", ti.Optional[ti.Bool])
-    .add_column("languages", ti.Optional[ti.Utf8])
-    .add_column("labels", ti.Optional[ti.Utf8])
-    .add_column("topics", ti.Optional[ti.Utf8])
-    .add_column("programmingLanguage", ti.Optional[ti.Utf8])
-    .add_column("scrapedData", ti.Optional[ti.Utf8])
+    schema=TableSchema.from_row_type(RepositoryDataRow)
 )
 
 yt_client.write_table(
