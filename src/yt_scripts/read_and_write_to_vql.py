50ccf5e224167947a6e4db62a8f33a21f4e750a3
Tobias Lindenbauer <tobias.lindenbauer@jetbrains.com>
2024-08-01 10:40:47 +0200

    Refactor: Working mapper running from WSL

--- src/yt_scripts/read_and_write_to_vql.py
+++ src/yt_scripts/read_and_write_to_vql.py
@@ -1,7 +1,13 @@
 import os
 import yt.wrapper as yt
-from mapper import DummyMapper
 
+
+def mapper_fun(input_row):
+    input_row['content'] = 'lol'
+    yield input_row
+
+
+@yt.respawn_in_docker("docker.io/library/python:3.10.12")
 def main():
     yt_client = yt.YtClient(proxy=os.environ["YT_PROXY"], token=os.environ["YT_TOKEN"])
 
@@ -9,10 +15,16 @@ def main():
     dst_table = "//home/ml4se/tobias_lindenbauer/data/demo_scraper_output"
 
     yt_client.run_map(
-        DummyMapper(),
+        mapper_fun,
         src_table,
         dst_table,
         job_count=3,
+        spec={
+            "mapper": {
+                "docker_image": "docker.io/library/python:3.10.12",
+            }
+        },
+        format=yt.YsonFormat(require_yson_bindings=False)
     )
 
 
