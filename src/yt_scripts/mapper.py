82819ae416a219511776b80b05cf2de95f73ef86
Tobias Lindenbauer <tobias.lindenbauer@jetbrains.com>
2024-08-01 16:28:17 +0200

    Update: Migrate to RepositoryDataMapper
    Updated the main function in read_and_write_to_vql.py to use RepositoryDataMapper instead of DummyMapper. Also adjusted the source and destination table paths and modified the mapper.py to include the implementation of RepositoryDataMapper.

--- src/yt_scripts/mapper.py
+++ src/yt_scripts/mapper.py
@@ -1,4 +1,4 @@
-from schemas import DummyRow
+from schemas import DummyRow, RepositoryDataRow
 from typing import Iterable
 import yt.wrapper
 
@@ -6,3 +6,9 @@ import yt.wrapper
 class DummyMapper(yt.wrapper.TypedJob):
     def __call__(self, row: DummyRow) -> Iterable[DummyRow]:
         yield DummyRow(content='I cannae belieeve eet')
+
+
+class RepositoryDataMapper(yt.wrapper.TypedJob):
+    def __call__(self, row: RepositoryDataRow) -> Iterable[RepositoryDataRow]:
+        row.scrapedData = '{dict of doom fear me}'
+        yield row
