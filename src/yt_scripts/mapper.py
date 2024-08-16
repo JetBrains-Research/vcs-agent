9afb40f9ecbd15a966b59d725817b9136237c4eb
Tobias Lindenbauer <tobias.lindenbauer@jetbrains.com>
2024-08-16 11:23:58 +0200

    Update: rename import and fix class inheritance formatting
    Updated the import statement to use an alias for consistency. Adjusted the class inheritance for `DummyMapper` and `RepositoryDataMapper` to ensure proper readability and maintenance.

--- src/yt_scripts/mapper.py
+++ src/yt_scripts/mapper.py
@@ -1,14 +1,16 @@
 from schemas import DummyRow, RepositoryDataRow
 from typing import Iterable
 import yt.wrapper
+import yt.wrapper as yt
 
 
-class DummyMapper(yt.wrapper.TypedJob):
+
+class DummyMapper(yt.TypedJob):
     def __call__(self, row: DummyRow) -> Iterable[DummyRow]:
         yield DummyRow(content='I cannae belieeve eet')
 
 
-class RepositoryDataMapper(yt.wrapper.TypedJob):
+class RepositoryDataMapper(yt.TypedJob):
     def __call__(self, row: RepositoryDataRow) -> Iterable[RepositoryDataRow]:
         row.scrapedData = '{dict of doom fear me}'
         yield row
