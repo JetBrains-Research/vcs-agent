ee23589a0ea7fec7315210963821daea6bfcb87a
Tobias Lindenbauer <tobias.lindenbauer@jetbrains.com>
2024-08-22 10:28:28 +0200

    Add: error handler for repo deletion and cleanup process
    Introduce a helper function to handle errors during directory removal when cleaning up repository folders. Also, enhance logging and ensure the working directory is reset after repository operations to avoid conflicts.

--- src/yt_scripts/mapper.py
+++ src/yt_scripts/mapper.py
@@ -1,4 +1,5 @@
 import os
+import shutil, stat
 import sys
 
 from schemas import DummyRow, RepositoryDataRow
@@ -17,15 +18,31 @@ class DummyMapper(yt.TypedJob):
         yield DummyRow(content='I cannae belieeve eet')
 
 
+def on_rm_error(func, path, exc_info):
+    """
+    This method is called by the shutil.rmtree() function when it encounters an error while trying to remove a directory
+     or a file. It is used to handle the error and continue with the removal operation.
+
+    Parameters:
+    - func: A function object that represents the removal function to be called again for the specific path.
+        It should accept a single parameter, which is the path to be removed.
+    - path: A string that represents the path of the directory or file that encountered the error.
+    - exc_info: Unused by the implementation.
+    """
+    os.chmod(path, stat.S_IWRITE)
+    func(path)
+
+
 class RepositoryDataMapper(yt.TypedJob):
     def __call__(self, row: RepositoryDataRow) -> Iterable[RepositoryDataRow]:
-        repository_path = "__".join(row.name.split("/"))
-
+        repository_folder = "__".join(row.name.split("/"))
+        path_to_repository = os.path.join('/slot/sandbox/repos', repository_folder)
         try:
             repo_instance = Repo.clone_from(f'https://github.com/{row.name}.git',
-                                            f'{repository_path}')
+                                            f'{path_to_repository}')
 
-            os.chdir(repository_path)
+            os.chdir(path_to_repository)
+            print(os.getcwd(), file=sys.stderr)
 
             if row.programming_language == 'kotlin':
                 programming_language = ProgrammingLanguage.KOTLIN
@@ -43,12 +60,22 @@ class RepositoryDataMapper(yt.TypedJob):
                                                  sliding_window_size=3)  # TODO hardcoded; Reduced sliding window size to 3
 
             repo_scraper.scrape()
-            print(repo_scraper.accumulator, file=sys.stderr)
 
             row.file_commit_gram_scenarios = str(repo_scraper.accumulator['file_commit_gram_scenarios'])
             row.merge_scenarios = str(repo_scraper.accumulator['merge_scenarios'])
             row.cherry_pick_scenarios = str(repo_scraper.accumulator['cherry_pick_scenarios'])
+
+            # Move back into tmpfs working directrory
+            os.chdir('..')
+
+            print('Current working directory: '+os.getcwd(), file=sys.stderr)
+            print(os.listdir('.'), file=sys.stderr)
+
+            shutil.rmtree(repository_folder, onerror=on_rm_error)
+
+            print(os.listdir('.'), file=sys.stderr)
         except Exception as e:
+            print(traceback.format_exc(), file=sys.stderr)
             row.error = traceback.format_exc()
             yield row  # Note that the column scrapedData could be empty here
         finally:
