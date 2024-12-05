24ad7cdd111b3df0d0eef0c2ad1a3c83190ef913
Tobias.Lindenbauer <tobias.lindenbauer@jetbrains.com>
2024-12-05 14:50:14 +0100

    UPDATE: Add argparse for dynamic table paths
    Added argparse to allow dynamic specification of source and destination table paths. This enables greater flexibility in handling tables without modifying the code directly.

--- src/yt_scripts/yt_maintenance_utils.py
+++ src/yt_scripts/yt_maintenance_utils.py
@@ -1,17 +1,15 @@
 import os
+import argparse
 import yt.wrapper as yt
 from dataclasses import asdict
 from yt.wrapper.schema import TableSchema
-
 from src.yt_scripts.mappers import ErrorFilteringMapper
-
 from src.yt_scripts.mappers import RepositoryDataMapper
 from src.yt_scripts.schemas import RepositoryDataRow
 import pandas as pd
 
 def parse_table_into_dataframe(table_path: str) -> pd.DataFrame:
     dataset = yt.read_table_structured(table=table_path, row_type=RepositoryDataRow)
-
     return pd.DataFrame([asdict(row) for row in dataset])
 
 def parse_table_into_csv_at(output_path: str, table_path: str):
@@ -32,13 +30,8 @@ def remove_duplicates_in(table_path: str, yt_client: yt.YtClient):
         input_stream=dataset_df.to_dict(orient="records"),
     )
 
-def handle_errors_in_dataset(yt_client: yt.YtClient):
-    src_table = "//home/ml4se/tobias_lindenbauer/data/scraper_output"
-    dst_table = "//home/ml4se/tobias_lindenbauer/data/cleaned_scraper_output"
-    dst_table_path = yt.TablePath(
-        dst_table,
-        schema=TableSchema.from_row_type(RepositoryDataRow)
-    )
+def handle_errors_in_dataset(yt_client: yt.YtClient, src_table: str, dst_table: str):
+    dst_table_path = yt.TablePath(dst_table, schema=TableSchema.from_row_type(RepositoryDataRow))
     yt_client.create('table', dst_table_path)
 
     yt_client.run_map(
@@ -46,45 +39,29 @@ def handle_errors_in_dataset(yt_client: yt.YtClient):
         source_table=src_table,
         destination_table=dst_table,
         job_count=10,
-
-        # Specifying this aligns the python versions (apparently Nebius clusters run on 3.8 by default),
-        # but Im pretty sure the YSON problem comes from the image not containing bindings.
         spec={
             "mapper": {
                 "docker_image": "docker.io/liqsdev/ytsaurus:python-3.10",
-                # Each job gets exactly one cpu, ensure high level of concurrency and efficient use of resources
                 "cpu_limit": 1
             },
         },
     )
 
-def run_repository_data_mapper(yt_client: yt.YtClient):
-    src_table = "//home/ml4se/tobias_lindenbauer/data/repositories_to_scrape"
-    dst_table = "//home/ml4se/tobias_lindenbauer/data/scraper_output"
-
+def run_repository_data_mapper(yt_client: yt.YtClient, src_table: str, dst_table: str):
     job_count = len(list(yt.read_table_structured(src_table, RepositoryDataRow)))
 
     yt_client.run_map(
         RepositoryDataMapper(sliding_window_size=3),
         src_table,
         dst_table,
-        # Set to the amount of total repositories in src_table to enqueue individual repos and
-        # spread the load evenly
         job_count=job_count,
-
-        # Specifying this aligns the python versions (apparently Nebius clusters run on 3.8 by default),
-        # but Im pretty sure the YSON problem comes from the image not containing bindings.
         spec={
             "mapper": {
                 "docker_image": "docker.io/liqsdev/ytsaurus:python-3.10",
-                # Each job can scale to at most 4 GiB of memory
                 "memory_limit": 4 * 1024 ** 3,
-                # The initial amount reserved for a job is thus 0.125 * 4000 = 500 MiB of memory
                 "memory_reserve_factor": 0.125,
-                # Support repositories up to a size of 1.5 GiB
                 "tmpfs_size": 1500 * 1024 ** 2,
                 "tmpfs_path": "repos",
-                # Each job gets exactly one cpu, ensure high level of concurrency and efficient use of resources
                 "cpu_limit": 1
             },
         },
@@ -92,10 +69,16 @@ def run_repository_data_mapper(yt_client: yt.YtClient):
 
 
 def main():
+    parser = argparse.ArgumentParser(description='Process some tables in YTsaurus.')
+    parser.add_argument('--src-table', type=str, help='Source table path', required=True)
+    parser.add_argument('--dst-table', type=str, help='Destination table path')
+    parser.add_argument('--csv-dataset-path', type=str, help='Path at which to persist CSV of dataset')
+    args = parser.parse_args()
+
     yt_client = yt.YtClient(proxy=os.environ["YT_PROXY"], token=os.environ["YT_TOKEN"],
                             config={'pickling': {'ignore_system_modules': True}})
-    # output_dir = os.path.join(os.getcwd(), 'data')
-    remove_duplicates_in('//home/ml4se/tobias_lindenbauer/data/scraper_output', yt_client)
+    remove_duplicates_in(args.src_table, yt_client)
+
 
 if __name__ == '__main__':
     main()
